6482cfd64dbcb1c5e896d8b9c6905cae5224a937
GitSmurff <43213623+GitSmurff@users.noreply.github.com>
2018-09-19 19:10:04 +0600

    laba 2

--- laba 2/laba2.3.py
+++ laba 2/laba2.3.py
@@ -1,5 +1,10 @@
 S1 = input("Введите строку: ").split()
-print(min(S1, key=len))
+S2 = input("Введите знак: ")
+for i in range (len(S1)):
+    for j in range (len(S1[i])):
+        if S1[i][j] == S2:
+            print("Ваше слово: ", S1[i])
+
 
 
 
